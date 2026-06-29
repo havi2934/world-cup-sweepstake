fddfd649b0522d73088a7d53294e6915634af5d0
havi2934 <160491591+havi2934@users.noreply.github.com>
2026-06-29 09:20:28 +0200

    Fixtures with times

--- data.js
+++ data.js
@@ -12,14 +12,102 @@ const sweepstake = {
     "South Africa"
   ],
 
-  fixtures: [
-    { date: "2026-06-29", home: "Brazil", away: "Japan" },
-    { date: "2026-06-29", home: "Germany", away: "Paraguay" },
-    { date: "2026-06-29", home: "Netherlands", away: "Morocco" },
-    { date: "2026-06-30", home: "France", away: "Sweden" },
-    { date: "2026-07-01", home: "England", away: "DR Congo" },
-    { date: "2026-07-01", home: "Belgium", away: "Senegal" },
-    { date: "2026-07-02", home: "Spain", away: "Austria" },
-    { date: "2026-07-02", home: "Portugal", away: "Croatia" }
-  ]
+ fixtures: [
+  // Round of 32
+
+  {
+    date: "2026-06-29",
+    time: "18:00",
+    home: "Brazil",
+    away: "Japan"
+  },
+  {
+    date: "2026-06-29",
+    time: "21:30",
+    home: "Germany",
+    away: "Paraguay"
+  },
+  {
+    date: "2026-06-30",
+    time: "02:00",
+    home: "Netherlands",
+    away: "Morocco"
+  },
+
+  {
+    date: "2026-06-30",
+    time: "18:00",
+    home: "Ivory Coast",
+    away: "Norway"
+  },
+  {
+    date: "2026-06-30",
+    time: "22:00",
+    home: "France",
+    away: "Sweden"
+  },
+  {
+    date: "2026-07-01",
+    time: "02:00",
+    home: "Mexico",
+    away: "Ecuador"
+  },
+
+  {
+    date: "2026-07-01",
+    time: "17:00",
+    home: "England",
+    away: "DR Congo"
+  },
+  {
+    date: "2026-07-01",
+    time: "21:00",
+    home: "Belgium",
+    away: "Senegal"
+  },
+  {
+    date: "2026-07-02",
+    time: "01:00",
+    home: "USA",
+    away: "Bosnia and Herzegovina"
+  },
+
+  {
+    date: "2026-07-02",
+    time: "20:00",
+    home: "Spain",
+    away: "Austria"
+  },
+  {
+    date: "2026-07-03",
+    time: "00:00",
+    home: "Portugal",
+    away: "Croatia"
+  },
+  {
+    date: "2026-07-03",
+    time: "04:00",
+    home: "Switzerland",
+    away: "Algeria"
+  },
+
+  {
+    date: "2026-07-03",
+    time: "19:00",
+    home: "Australia",
+    away: "Egypt"
+  },
+  {
+    date: "2026-07-03",
+    time: "23:00",
+    home: "Argentina",
+    away: "Cape Verde"
+  },
+  {
+    date: "2026-07-04",
+    time: "02:30",
+    home: "Colombia",
+    away: "Ghana"
+  }
+]
 };
